fca6db14e29574668631e67ced33b92c7c9c1cf9
Stas Lekontsev <stas.lekontsv@gmail.com>
2022-07-22 00:23:43 +0400

    Rename variables

--- Trade.Service/CandlestickPatterns/Hammer.cs
+++ Trade.Service/CandlestickPatterns/Hammer.cs
@@ -5,10 +5,21 @@ namespace Trade.Service.CandlestickPatterns
 {
     internal class Hammer : ICandlestickPattern
     {
+        // What percentage is the candlestick body of the
+        // larger shadow (upper/lower never mind).
         public decimal MaxBodyLengthPercentage { get; set; }
+        
         public decimal MinBodyLengthPercentage { get; set; }
         public decimal MaxUpperShadowLengthPercentage { get; set; }
         public decimal MaxLowerShadowLengthPercentage { get; set; }
+
+        // The idea behind the Hammer pattern here
+        // is to provide 5 candles where the first
+        // 3 candlesticks is in front of the Hammer
+        // and the last candlestick staying after the 
+        // potential Hammer in the array should confirm
+        // the pattern.
+        // Nonetheless it can be setup by your own.
         public int WindowLength { get; set; } = 5;
 
         public MarketMovement GetValue(IList<DataBar> data)
@@ -27,12 +38,14 @@ namespace Trade.Service.CandlestickPatterns
 
         private bool HasPattern(IList<DataBar> data)
         {
-            var midDataBarIndex = data.Count / 2;
-            var prevDataBarIndex = midDataBarIndex--;
-            var nextDataBarIndex = midDataBarIndex++;
+            var lastDataBarIndex = data.Count;
 
-            if (data[midDataBarIndex].RealBody / data[midDataBarIndex].TotalLength < MaxBodyLengthPercentage &&
-                data[prevDataBarIndex].RealBody )
+            // The potential hammer is in front
+            // of the last candlestick in the array.
+            var potentialHammerIndex = lastDataBarIndex - 1;
+
+            if (data[potentialHammerIndex].RealBody / data[potentialHammerIndex].TotalLength < MaxBodyLengthPercentage &&
+                )
                 return true;
 
             return false;
